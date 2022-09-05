b0e92e7c4cfac23e372a90eba9665dcfecffead8
oud <mo.oudadda@gmail.com>
2022-09-05 15:38:47 +0200

    Add script to access DVC version data

---
+++ Lab_DataVersionControl/access_dvc_data.py
@@ -0,0 +1,18 @@
+# Get url from DVC
+import pandas as pd
+import dvc.api
+
+path='Lab_DataVersionControl/data/wine_original.csv'
+repo='https://github.com/Mo-Oudadda/mlops_Lab'
+version = 'v1'
+
+data_url = dvc.api.get_url(
+  path=path,
+  repo=repo,
+  rev = version
+  )
+
+data = pd.read_csv(data_url, sep=";")
+print(data)
+
+
